b774425570f5a95eb93945f0f42dc39f800f1d65
artur borges <inustec@gmail.com>
2024-07-03 16:58:14 -0300

    Normalize line endings and re-add files

---
+++ src/main/java/com/filosodevRH2/apprecursoshumanos/ApprecursoshumanosApplication.java
@@ -0,0 +1,13 @@
+package com.filosodevRH2.apprecursoshumanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprecursoshumanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprecursoshumanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filosodevRH2/apprecursoshumanos/ApprecursoshumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filosodevRH2.apprecursoshumanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprecursoshumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
